0ab054a1b8a5ff464712a270ce8a80eaa68688c9
MacbookM1 <changjiageng@u.nus.edu>
2025-12-24 09:56:51 +0800

    tidy re-eval script

--- src/reeval.py
+++ src/reeval.py
@@ -9,57 +9,73 @@ from openevals.prompts import CORRECTNESS_PROMPT
 from langchain_openai import ChatOpenAI
 import json
 
-# 1. Create and/or select your dataset
-ls_client = Client()
-
-llm = ChatOpenAI(
-    model=os.environ.get("EVAL_MODEL_ID"),
-    temperature=0.,
-)
-
-# Load the failed examples JSON file
-with open(f"../responses/microdocs/{os.environ.get('LANGSMITH_PROJECT')}.json", 'r') as f:
-    failed_examples = json.load(f)
-
-def target(inputs: dict) -> dict:
-    print('dataset input: ',inputs)
-    # Find matching input and return the output
-    for example in failed_examples:
-        if example.get('inputs').get('question') == inputs['question']:
-            print(example)
-            return {'answer': example.get('output', 'key does not exist')}
-    
-    # Return empty if no match found
-    return {'answer':'I couldn\'t find the answer'}
-
-# a correctness score from 0 to 1, where 1 is the best
-def scorer(inputs: dict, outputs: dict, reference_outputs: dict):
-    evaluator = create_llm_as_judge(
-        prompt=CORRECTNESS_PROMPT,
-        judge=llm,
-        feedback_key="score",
-        continuous=True,
-    )
-    eval_result = evaluator(
-        inputs=inputs,
-        outputs=outputs,
-        reference_outputs=reference_outputs
-    )
-    return eval_result
-
 if __name__ == "__main__":
-    # Create langsmith dataset from failed examples and copy its ID here
-    eval_dataset_name = "ds-husky-platinum-75"
 
+    os.chdir(os.path.join(os.path.dirname(__file__), "../responses/microdocs"))
+
+    EVAL_DATASET_NAME = input("Enter the evaluation dataset name: ") or "myegpt-22Dec25"
+    LLM_MODEL_ID = input(f"Enter LLM model ID: (default) {os.environ.get('MODEL_ID')}") or os.environ.get("MODEL_ID")
+    EVAL_MODEL_ID = input(f"Enter the evaluation model ID: (default) {os.environ.get('EVAL_MODEL_ID')}") or os.environ.get("EVAL_MODEL_ID")
+    OUTPUT_JSON = input(f"Enter the output JSON file name: (default) {EVAL_MODEL_ID}-{LLM_MODEL_ID}.json") or f"EMBED_{EVAL_MODEL_ID}_LLM_{LLM_MODEL_ID}.json"
+
+    # 1. Create and/or select your dataset
+    ls_client = Client()
+
+    llm = ChatOpenAI(
+        model=EVAL_MODEL_ID,
+        temperature=0.,
+    )
+
+    def target(inputs: dict) -> dict:
+        global examples_to_evaluate
+        print('dataset input: ',inputs)
+        # Find matching input and return the output
+        for example in examples_to_evaluate:
+            if example.get('inputs').get('question') == inputs['question']:
+                print(example)
+                return {'answer': example.get('output', 'key does not exist')}
+        
+        # Return empty if no match found
+        return {'answer':'I couldn\'t find the answer'}
+
+    # a correctness score from 0 to 1, where 1 is the best
+    def scorer(inputs: dict, outputs: dict, reference_outputs: dict):
+        evaluator = create_llm_as_judge(
+            prompt=CORRECTNESS_PROMPT,
+            judge=llm,
+            feedback_key="score",
+            continuous=True,
+        )
+        eval_result = evaluator(
+            inputs=inputs,
+            outputs=outputs,
+            reference_outputs=reference_outputs
+        )
+        return eval_result
+
+    # Load the failed examples JSON file
+    eval_file = input("Enter the path to the response JSON file:")
+    with open(eval_file, 'r') as f:
+        examples_to_evaluate = json.load(f)
+    
     # 4. Run evaluation
     experiment = ls_client.evaluate(
         target,
-        data=eval_dataset_name,
+        data=EVAL_DATASET_NAME,
         evaluators=[scorer],
-        experiment_prefix="re-evaluation",
+        experiment_prefix=os.environ.get("LANGSMITH_PROJECT"),
+        metadata={
+                    'llm_model': LLM_MODEL_ID,
+                    'eval_model': EVAL_MODEL_ID,
+                },
         upload_results=True
     )
 
     # 5. Export results to a JSON file
     df = experiment.to_pandas()
-    df[['inputs.question','reference.answer','reference.scoring','outputs.answer','feedback.score']].to_json('eval_results.json',orient='index',indent=2)
+    df[['inputs.question',
+        'reference.answer',
+        'reference.scoring',
+        'outputs.answer',
+        'feedback.score']]\
+            .to_json(OUTPUT_JSON,orient='index',indent=2)
