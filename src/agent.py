6a19a0eecf38d8212953e5e4c0fa58f129e959f8
jiagengMBP <jiageng1997@gmail.com>
2026-09-12 23:20:22 +0800

    remove document search tool

--- src/agent.py
+++ src/agent.py
@@ -8,7 +8,7 @@ from langgraph.prebuilt import create_react_agent
 import matplotlib
 matplotlib.use('Agg') # non-interactive backend
 
-from tools import document_search_tool, convert_gene_tool, gene_metadata_tool, gene_level_copy_number_tool, cox_regression_base_data_tool, langchain_query_sql_tool, python_repl_tool, python_execute_sql_query_tool, display_plot_tool, generate_graph_filepath_tool
+from tools import convert_gene_tool, gene_metadata_tool, gene_level_copy_number_tool, cox_regression_base_data_tool, langchain_query_sql_tool, python_repl_tool, python_execute_sql_query_tool, display_plot_tool, generate_graph_filepath_tool
 from llm_utils import universal_chat_model
 
 # Create a system message for the agent
@@ -37,7 +37,7 @@ async def send_init_prompt(app:FastAPI):
     
     graph = create_react_agent(
         model=llm,
-        tools=[document_search_tool, convert_gene_tool, gene_metadata_tool, gene_level_copy_number_tool, cox_regression_base_data_tool, langchain_query_sql_tool, python_repl_tool, python_execute_sql_query_tool, generate_graph_filepath_tool, display_plot_tool],
+        tools=[convert_gene_tool, gene_metadata_tool, gene_level_copy_number_tool, cox_regression_base_data_tool, langchain_query_sql_tool, python_repl_tool, python_execute_sql_query_tool, generate_graph_filepath_tool, display_plot_tool],
         checkpointer=InMemorySaver(),
     )
     system_message = create_system_message()
